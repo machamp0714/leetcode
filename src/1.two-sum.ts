728ac5925025ad40fc6b13cfdf418ae0ab1aadce
machamp0714 <rateliasudaisuki@gmail.com>
2021-02-28 23:26:33 +0900

    better solution

--- src/1.two-sum.ts
+++ src/1.two-sum.ts
@@ -5,29 +5,41 @@
  */
 
 // @lc code=start
+// function twoSum(nums: number[], target: number): number[] {
+//   let result: number[] = [];
+
+//   for (let i = 0; i < nums.length; i++) {
+//     const num = nums[i];
+
+//     for (let j = 0; j < nums.length; j++) {
+//       if (i === j) {
+//         continue;
+//       }
+//       const other_num = nums[j];
+//       if (num + other_num === target) {
+//         result.push(i);
+//         result.push(j);
+//       }
+//       if (result.length) {
+//         return result;
+//       }
+//     }
+//   }
+//   return result;
+// };
 function twoSum(nums: number[], target: number): number[] {
-  let result: number[] = [];
+  const map: Map<number, number> = new Map();
 
   for (let i = 0; i < nums.length; i++) {
-    const num = nums[i];
-
-    for (let j = 0; j < nums.length; j++) {
-      if (i === j) {
-        continue;
-      }
-      const other_num = nums[j];
-      if (num + other_num === target) {
-        result.push(i);
-        result.push(j);
-      }
-      if (result.length) {
-        return result;
-      }
+    if (map.has(nums[i])) {
+      return [map.get(nums[i])!, i];
     }
+    map.set(target - nums[i], i);
   }
-  return result;
-};
+  return [];
+}
 // @lc code=end
 
-twoSum([2, 7, 11, 15], 9);
-twoSum([3, 3], 6);
+console.log(twoSum([2, 7, 11, 15], 9));
+console.log(twoSum([3, 3], 6));
+console.log(twoSum([3, 2, 4], 6));
