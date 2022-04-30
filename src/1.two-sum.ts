37e2685a9a974b6233dace0f6a285fa008a2196d
machamp0714 <machamp0714@gmail.com>
2022-04-30 09:30:37 +0900

    add: vscode

--- src/1.two-sum.ts
+++ src/1.two-sum.ts
@@ -5,28 +5,6 @@
  */
 
 // @lc code=start
-// function twoSum(nums: number[], target: number): number[] {
-//   let result: number[] = [];
-
-//   for (let i = 0; i < nums.length; i++) {
-//     const num = nums[i];
-
-//     for (let j = 0; j < nums.length; j++) {
-//       if (i === j) {
-//         continue;
-//       }
-//       const other_num = nums[j];
-//       if (num + other_num === target) {
-//         result.push(i);
-//         result.push(j);
-//       }
-//       if (result.length) {
-//         return result;
-//       }
-//     }
-//   }
-//   return result;
-// };
 function twoSum(nums: number[], target: number): number[] {
   const map: Map<number, number> = new Map();
 
