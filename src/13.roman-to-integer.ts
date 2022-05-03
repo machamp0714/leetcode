8a0ce7a8a06518bf365e001c97e0b42e3156737d
machamp0714 <machamp0714@gmail.com>
2022-05-03 10:46:24 +0900

    refactor: roman-to-integers

--- src/13.roman-to-integer.ts
+++ src/13.roman-to-integer.ts
@@ -5,19 +5,56 @@
  */
 
 // @lc code=start
-function romanToInt(s: string): number {
-  let [ss, array] = convertToRomanArray(spacialConverter(), s);
-  [ss, array] = convertToRomanArray(basicConverter(), ss, array);
-  return array.reduce((pre, cur) => pre + cur, 0);
+const converter: { [key: string]: number } = {
+  IV: 4,
+  IX: 9,
+  XL: 40,
+  XC: 90,
+  CD: 400,
+  CM: 900,
+  I: 1,
+  V: 5,
+  X: 10,
+  L: 50,
+  C: 100,
+  D: 500,
+  M: 1000,
 };
 
-function convertToRomanArray(converter: object, s: string, a: number[] = []): [string, number[]] {
+const tokens = Object.keys(converter);
+
+function romanToInt(s: string): number {
+  function next(): string {
+    for (const token of tokens) {
+      if (s.startsWith(token)) {
+        s = s.replace(token, '');
+        return token;
+      }
+    }
+
+    throw new Error(`Unknown token: ${s}`);
+  }
+
+  let chars: string[] = [];
+
+  while (s.length) { chars.push(next()) }
+
+  return chars.reduce((sum, char) => {
+    return sum += converter[char]
+  }, 0);
+}
+
+function convertToRomanArray(
+  converter: object,
+  s: string,
+  a: number[] = []
+): [string, number[]] {
   let dup = s;
   while (includesKey(converter, dup)) {
     for (const [key, value] of Object.entries(converter)) {
       if (dup.includes(key)) {
         a.push(value);
-        dup = dup.replace(key, '');
+        dup = dup.replace(key, "");
       }
     }
   }
@@ -25,64 +62,24 @@ function convertToRomanArray(converter: object, s: string, a: number[] = []): [s
 }
 
 function includesKey(converter: object, s: string) {
-  return Object.keys(converter).some(key => s.includes(key));
+  return Object.keys(converter).some((key) => s.includes(key));
 }
 
-function basicConverter() {
-  return {
-    'I': 1,
-    'V': 5,
-    'X': 10,
-    'L': 50,
-    'C': 100,
-    'D': 500,
-    'M': 1000
-  }
-}
-
-function spacialConverter() {
-  return {
-    'IV': 4,
-    'IX': 9,
-    'XL': 40,
-    'XC': 90,
-    'CD': 400,
-    'CM': 900
-  }
-}
 // @lc code=end
-romanToInt('I');
-romanToInt('IV');
-romanToInt('V');
-romanToInt('IX');
-romanToInt('X');
-romanToInt('XL');
-romanToInt('XLIX');
-romanToInt('CDXCIX');
-romanToInt('L');
-romanToInt('XC');
-romanToInt('C');
-romanToInt('CD');
-romanToInt('D');
-romanToInt('CM');
-romanToInt('CMXCIX');
-romanToInt('M');
-romanToInt('MMMCMXCIX');
-// 1 = I
-// 4 = IV
-// 5 = V
-// 9 = IX
-// 10 = X
-// 40 = XL
-// 49 = XLIX
-// 50 = L
-// 90 = XC
-// 100 = C
-// 400 = CD
-// 499 = CDXCIX
-// 500 = D
-// 900 = CM
-// 999 = CMXCIX
-// 1000 = M
-// 1001 = MI
-// 3999 = MMMCMXCIX = ['MMM', 'CM', 'XC', 'IX'] = [3000, 900, 90, 9] = 3999
+romanToInt("I");
+romanToInt("IV");
+romanToInt("V");
+romanToInt("IX");
+romanToInt("X");
+romanToInt("XL");
+romanToInt("XLIX");
+romanToInt("CDXCIX");
+romanToInt("L");
+romanToInt("XC");
+romanToInt("C");
+romanToInt("CD");
+romanToInt("D");
+romanToInt("CM");
+romanToInt("CMXCIX");
+romanToInt("M");
+romanToInt("MMMCMXCIX");
